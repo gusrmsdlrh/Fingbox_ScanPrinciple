549592472acada4a131609ffffdc08af84ff12c5
JJoGen <40857478+gusrmsdlrh@users.noreply.github.com>
2019-05-07 15:12:55 +0900

    Update UDP_SSDP.py

--- UDP_SSDP.py
+++ UDP_SSDP.py
@@ -1,13 +1,10 @@
 from scapy.all import *
-
-spoofedIPsrc=""
-SSDPserver="239.255.255.250"
  
 payload = "M-SEARCH * HTTP/1.1\r\n" \
-"HOST:"+SSDPserver+":1900\r\n" \
-"ST:upnp:rootdevice\r\n" \
+"HOST: 239.255.255.250:1900\r\n" \
 "MAN: \"ssdp:discover\"\r\n" \
-"MX:2\r\n\r\n"
+"MX: 1\r\n"\
+"ST: upnp:rootdevice\r\n\r\n"
  
-ssdpRequest = IP(src=spoofedIPsrc,dst=SSDPserver) / UDP(sport=1900, dport= 1900) / payload
-sr1(ssdpRequest)
+ssdpRequest = IP(dst='239.255.255.250') / UDP(sport=1900, dport= 1900) / payload
+sr1(ssdpRequest, verbose=0, timeout=time)
