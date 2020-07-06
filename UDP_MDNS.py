e26fecdbc7f6ae07e1caefdd1cba91118c029c02
JoGeun <40857478+gusrmsdlrh@users.noreply.github.com>
2020-07-06 11:15:54 +0900

    Update UDP_MDNS.py

--- UDP_MDNS.py
+++ UDP_MDNS.py
@@ -1,4 +1,17 @@
 from scapy.all import *
 
-mdnsRequest = pk=IP(dst='224.0.0.251')/UDP(sport=5353, dport='mdns')/DNS(opcode='QUERY', rd=0,rcode='ok', qdcount=1, qd=DNSQR(qname='_http._tcp.local.',qtype='PTR', qclass='IN')/DNSQR(qname='_services._dns-sd._udp.local.',qtype='PTR', qclass='IN'))
-response=sr1(mdnsRequest, verbose=0, timeout=1)
+mdnsRequest= pk=IP(dst='224.0.0.251')/UDP(sport=5353, dport=5353)/DNS(opcode='QUERY',qdcount=11,qd=DNSQR(qname='_http._tcp.local.', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_rtsp._tcp.local.', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_https._tcp.local.', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_services._dns-sd._udp.local', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_svnp._tcp.local', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_adisk._tcp.local', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_afpovertcp._tcp.local', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_workstation._tcp.local', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_CGI._tcp.local', qtype='PTR', qclass='IN')
+			/DNSQR(qname=reverse[3]+'.'+reverse[2]+'.'+reverse[1]+'.'+reverse[0]+'.in-addr.arpa', qtype='PTR', qclass='IN')
+			/DNSQR(qname='_psia._tcp.local', qtype='PTR', qclass='IN'))
+	print(Fore.CYAN+"[+] Send : MDNS Packet REQUEST"+Fore.RESET)
+	response=sr(mdnsRequest, verbose=0, timeout=time)
+	print response
+
